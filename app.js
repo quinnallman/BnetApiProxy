6c6df0967afbf108e817df1dc056808fa01de057
Greg Roberts <groberts@gmail.com>
2024-08-10 00:44:36 -0400

    Move bnet stuff into a helper

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const axios = require('axios')
 const cors = require('cors')
 const express = require('express')
-const makeBnetURL = require('./helpers')
+const helpers = require('./helpers')
 
 const app = express()
 const port = 3000
@@ -9,7 +9,7 @@ const port = 3000
 app.use(cors())
 
 const client = axios.create({
-  baseURL: makeBnetURL(process.env.BNET_REGION),
+  baseURL: helpers.makeBnetURL(process.env.BNET_REGION),
 })
 
 let authResponse = {};
@@ -19,7 +19,7 @@ client.interceptors.request.use(async config => {
   const now = Date.now()
 
   if (!authResponse.access_token || now > authExpiry) {
-    authResponse = await getAuthToken()
+    authResponse = await helpers.getAuthToken(process.env.BNET_CLIENT_ID, process.env.BNET_CLIENT_SECRET)
     authExpiry = now + (authResponse.expires_in - 60) * 1000
   }
 
@@ -29,21 +29,24 @@ client.interceptors.request.use(async config => {
   return Promise.reject(error)
 })
 
-getAuthToken = async () => {
-  const basic_auth = 'Basic ' + Buffer.from(process.env.BNET_CLIENT_ID + ':' + process.env.BNET_CLIENT_SECRET).toString('base64')
-
-  return axios({
-    url: 'https://oauth.battle.net/token?grant_type=client_credentials',
-    method: 'POST',
-    headers: {
-      'Authorization': basic_auth,
-    },
-  })
-  .then(resp => resp.data)
-}
-
 app.get('/achievements', async (_req, res) => {
   return await client.get('/data/wow/achievement/index?namespace=static-us&locale=en_US')
+    .then(resp => {
+      res.send(resp.data)
+      return resp.data
+    })
+})
+
+app.get('/transmog/sets/:tsid', async (req, res) => {
+  return await client.get('/data/wow/item-appearance/set/' + req.params.tsid + '?namespace=static-us&locale=en_US')
+    .then(resp => {
+      res.send(resp.data)
+      return resp.data
+    })
+})
+
+app.get('/transmog/item/:iid', async (req, res) => {
+  return await client.get('/data/wow/item-appearance/' + req.params.iid + '?namespace=static-us&locale=en_US')
     .then(resp => {
       res.send(resp.data)
       return resp.data
--- helpers.js
+++ helpers.js
@@ -1,7 +1,25 @@
+const axios = require('axios')
+
 const makeBnetURL = (region) => {
   if (region === 'cn')
     return 'https://gateway.battlenet.com.cn'
   return 'https://' + region + '.api.blizzard.com'
 }
 
-module.exports = makeBnetURL
+const getAuthToken = async (client_id, client_secret) => {
+  const basic_auth = 'Basic ' + Buffer.from(process.env.BNET_CLIENT_ID + ':' + process.env.BNET_CLIENT_SECRET).toString('base64')
+
+  return axios({
+    url: 'https://oauth.battle.net/token?grant_type=client_credentials',
+    method: 'POST',
+    headers: {
+      'Authorization': basic_auth,
+    },
+  })
+  .then(resp => resp.data)
+}
+
+module.exports = {
+  makeBnetURL,
+  getAuthToken,
+}
